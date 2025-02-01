3466d81f572f93c5bee852354ebe6902d7256b2e
Zia <ziakayani.official@gmail.com>
2025-02-01 07:21:06 -0800

    component creation and react-router setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,26 @@
 
 import './App.css'
 
+import {BrowserRouter as Router ,Routes, Route, Navigate} from "react-router-dom";
+import Landing from './components/Landing';
+import Dashboard from './components/dashboard/Dashboard';
+import Register from './components/Register';
+import Login from './components/Login';
+
 function App() {
 
   return (
     <>
-    <div>
-      <p>Hello world</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/landing' element={<Landing />} />
+
+      </Routes>
+    </Router>
+    
     </>
   )
 }
